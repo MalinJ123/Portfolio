756cbdaf02c9a5f4b55458b7246d525f065d27a1
MalinJ123 <malin--jonsson@hotmail.com>
2024-04-14 14:05:29 +0200

    started with section 2 on start page, aboute me

--- src/routes/Start.jsx
+++ src/routes/Start.jsx
@@ -1,4 +1,5 @@
 import working from "../images/me-working.jpg";
+import me from "../images/me.jpg"
 import "../styles/start.css";
 function Start() {
   return (
@@ -22,6 +23,38 @@ function Start() {
 
         <div className="wrapper__container">
           <img src={working} className="hero-img" alt="When i'm programming" />
+        </div>
+      </section>
+      <section className="about">
+        <div className="about__container">
+          <img className="img" src={me} alt="" />
+        </div>
+        <div className="about__container">
+          <h1>Abolut Me</h1>
+          <article>
+            Under min yrkesresa insåg jag mitt intresse för IT och
+            webbutveckling. Detta ledde mig till min pågående utbildning som
+            Frontend-utvecklare. Där jag fokuserar på att fördjupa mina
+            kunskaper inom UI/UX, HTML, CSS, JavaScript, React, +
+            Backend-utveckling med Node.js, AWS, Firebase, MongoDb etc
+          </article>
+
+          <nav>Skills</nav>
+          <nav>Experience</nav>
+          <nav>Education</nav>
+
+          <article>
+           <div>
+           <h5>UI/UX </h5>
+            <p>Designing Web/app interfaces</p>
+            
+            <div>
+            <h5>Frontend Develpment</h5>
+            <p>HTML, CSS, JavaScript, React, TypeScript, Node.js, Responsive Design, Git, Npm package</p>
+            
+            </div>
+           </div>
+          </article>
         </div>
       </section>
     </>
