a1756f1b8707f389a768df1438fa33335279a189
MalinJ123 <malin--jonsson@hotmail.com>
2024-06-11 11:31:13 +0200

    starting to get the portfolio view tha way I want, with a onClick function

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <Header/>
     <Start/>
     <About/>
-    {/* <Portfolio/> */}
+    <Portfolio/>
     </>
   )
 }
--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -1,48 +1,86 @@
+import React, { useState, useEffect } from "react";
 import calender from "../images/portfolio/calernder.png";
 import hangman from "../images/portfolio/hangman.png";
 import pokemon from "../images/portfolio/pokemon.png";
-
 import "../styles/portfolio.css";
 
-function Portfolio(){
+function Portfolio() {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    return(
-        <>
-          <section className="portfolio">
-          <h2 className="portfolio-h2">Portfolio</h2>
+  useEffect(() => {
+    document.querySelectorAll(".portfolio__container-card").forEach((card) => {
+      const images = card.querySelectorAll(".portfolio-img");
+      showImage(images, currentIndex);
+    });
+  }, [currentIndex]);
 
-          {/* start of portfolio cards */}
-          <div className="portfolio__container">
-            <div className="portfolio__container-card">
-              <h2> hej</h2>
-              <img className="portfolio-img" src={calender} alt="" />
-              <p>description</p>
-            </div>
+  function showImage(images, index) {
+    images.forEach((img, idx) => {
+      img.classList.toggle("hidden", idx !== index);
+    });
+  }
 
-            <div className="portfolio__container-card">
-              <h2> hej</h2>
-              <img className="portfolio-img" src={pokemon} alt="" />
-              <p>description</p>
-            </div>
+  function nextImage() {
+    setCurrentIndex((currentIndex) => {
+      if (currentIndex === 2) {
+        return 0;
+      }
+      return currentIndex + 1;
+    });
+  }
 
-            <div className="portfolio__container-card">
-              <h2> hej</h2>
-              <img className="portfolio-img" src={hangman} alt="" />
-              <p>description</p>
-            </div>
-            <div className="portfolio__container-card">
-              <h2> hej</h2>
-              <img className="portfolio-img" src={calender} alt="" />
-              <p>description</p>
-            </div>
+  function prevImage() {
+    setCurrentIndex((currentIndex) => {
+      if (currentIndex === 0) {
+        return 2;
+      }
+      return currentIndex - 1;
+    });
+  }
 
-            <div className="portfolio__container-card">
-              <h2> hej</h2>
-              <img className="portfolio-img" src={pokemon} alt="" />
-              <p>description</p>
-            </div>
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio-h2">Portfolio</h2>
+      <div className="portfolio__container">
+        <div className="portfolio__container-card">
+          <h2 className="portfolio__card-title">Kalender app</h2>
+          <div className="portfolio-img-container">
+            <img className="portfolio-img" src={calender} alt="calender" />
+            <img className="portfolio-img hidden" src={hangman} alt="hangman" />
+            <img className="portfolio-img hidden" src={pokemon} alt="pokemon" />
+            <button className="prev" onClick={prevImage}>
+              ❮
+            </button>
+            <button className="next" onClick={nextImage}>
+              ❯
+            </button>
           </div>
-        </section>
-        </>
-    )
-} export default Portfolio;
+          <p className ="portfolio__card-description">This calender app was made from 
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet dicta vitae repellendus adipisci explicabo, qui commodi reiciendis reprehenderit perspiciatis aut velit consequuntur odit repudiandae, provident accusamus aperiam. Velit, minima suscipit. </p>
+        
+        </div>
+
+
+        <div className="portfolio__container-card">
+          <h2 className="portfolio__card-title">Kalender app</h2>
+          <div className="portfolio-img-container">
+            <img className="portfolio-img" src={calender} alt="calender" />
+            <img className="portfolio-img hidden" src={hangman} alt="hangman" />
+            <img className="portfolio-img hidden" src={pokemon} alt="pokemon" />
+            <button className="prev" onClick={prevImage}>
+              ❮
+            </button>
+            <button className="next" onClick={nextImage}>
+              ❯
+            </button>
+          </div>
+          <p className ="portfolio__card-description">This calender app was made from 
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet dicta vitae repellendus adipisci explicabo, qui commodi reiciendis reprehenderit perspiciatis aut velit consequuntur odit repudiandae, provident accusamus aperiam. Velit, minima suscipit. </p>
+        
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Portfolio;
